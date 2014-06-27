bc5bb1d92cbcd6731236e9bda978da2fa5960714
dead-persimmon <kosyrin@gmail.com>
2014-06-27 15:26:07 -0400

    stuff

--- crawler-test.py
+++ crawler-test.py
@@ -1,3 +1,3 @@
 ﻿from os import environ as environment
 
-open(environment['OPENSHIFT_REPO_DIR'] + '/crawler-test.log', 'a').close()
+open(environment['OPENSHIFT_REPO_DIR'] + 'crawler-test.log', 'a').close()
--- parsers.py
+++ parsers.py
@@ -1,9 +1,12 @@
 ﻿import re
 
 def nyaa_rss(tree):
-	for item in tree.findall('.//itemzoz'):
-		print(item)
-		#title, category, torrent_link, details_link, meta_string, date = (child.text for child in item.getchildren())
+	for item in tree.findall('.//item'):
+		try:
+			title, category, torrent_link, details_link, meta_string, date = (child.text for child in item.getchildren())
+		except ValueError:
+			continue
+		print(title)
 		#result = re.match('^([0-9]+) seeder\(s\), ([0-9]+) leecher\(s\), ([0-9]+) download\(s\)', meta_string)
 		#result = result.groups()
 		#if len(result) == 3:
