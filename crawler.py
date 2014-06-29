e0bf675e56175d5f2372ee96125df69ed5378bff
dead-persimmon <kosyrin@gmail.com>
2014-06-29 15:58:21 -0400

    crawler

--- crawler.py
+++ crawler.py
@@ -1,13 +1,20 @@
-﻿exit()
-
+﻿import time
 from datetime import datetime
+
+import os
 
 from os.path import isfile as is_file
 from os import remove as del_file
 from os import linesep as line_sep
 
 from urllib.request import urlopen as open_url
-from xml.etree.ElementTree import parse as parse_tree
+import xml.etree.ElementTree as ET
+
+import re
+
+from bson.objectid import ObjectId as ObjectID
+
+import hashlib
 
 lock_file_name = 'crawler.lock'
 log_file_name = 'crawler.log'
@@ -24,25 +31,38 @@ if is_file(lock_file_name):
 
 open(lock_file_name, 'a').close()
 
-def nyaa_rss(tree):
+data = []
+
+def nyaa_rss_parser(tree):
 	counter = 0
 	for item in tree.findall('.//item'):
 		try:
-			title, category, torrent_link, details_link, meta_string, date = (child.text for child in item.getchildren())
-			seeders, leechers, downloads = re.match('^([0-9]+) seeder\(s\), ([0-9]+) leecher\(s\), ([0-9]+) download\(s\)', meta_string).groups()
+			title, category, torrent_link, details_link, meta_string, date_string = (child.text for child in item.getchildren())
+			s, l, d = re.match('^([0-9]+) seeder\(s\), ([0-9]+) leecher\(s\), ([0-9]+) download\(s\)', meta_string).groups()
+			id = ObjectID('{:0>24}'.format(hex(int(re.match('.*=([0-9]+)$', torrent_link).group(1)))[2:]))
+			timestamp = time.strptime(date_string, '%a, %d %b %Y %H:%M:%S %z')
+			data.append({'_id': id, 'title': title, 'torrent_link': torrent_link, 'seeders': s, 'leechers': l, 'downloads': d, 'timestamp': timestamp})
 			counter += 1
 		except Exception as exception: #ValueError
 			log(exception)
-			continue
 	log('Collected %d items from Nyaa.' % counter)
 
-init = [('http://www.nyaa.se/?page=rss&cats=1_37&filter=2&offset=%d', nyaa_rss)]
+sites = [('http://www.nyaa.se/?page=rss&cats=1_37&filter=2&offset=%d', nyaa_rss_parser, range(1, 3))]
 
-for site, parser in init:
+for site, site_parser, page_offsets in sites:
 	try:
-		tree = parse_tree(open_url(site % 1))
-		parser(tree)
+		for page_offset in page_offsets:
+			tree = ET.parse(open_url(site % page_offset))
+			site_parser(tree)
 	except Exception as exception:
 		log(exception)
-	
+
+from pymongo import MongoClient
+
+with MongoClient('mongodb://localhost:27017/') as client:
+#with MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL']) as client:
+	collection = client.sashok.torrents
+	for d in data:
+		collection.save(d)
+
 del_file(lock_file_name)
--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(name='App', version='1.0', description='OpenShift App', author='Sashok', author_email='kosyrin@gmail.com', url='http://www.python.org/sigs/distutils-sig/')
