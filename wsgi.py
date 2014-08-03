bcd17a95a02e4f044a002a5b70808e4e2d11213f
dead-persimmon <kosyrin@gmail.com>
2014-08-03 07:43:44 -0400

    still getting Flask to run...

--- wsgi.py
+++ wsgi.py
@@ -1,61 +1,68 @@
 #!/usr/bin/env python
 
-import debug, os, cgi, html
+from flask import Flask
+app = Flask(__name__)
 
-BAD_REQUEST = ('400 Bad Request', [('Content-Type', 'text/html')], 'Nope!')
+@app.route('/')
+def index():
+    return 'Testing—'
 
-def request_root_page(env):
-    response_status = '200 OK'
-    response_headers = [('Content-Type', 'text/html')]
-    if debug.local_run(): response_body = open('torrents_by_day.angular', 'rb').read()
-    else: response_body = open(env['OPENSHIFT_REPO_DIR'] + 'torrents_by_day.angular', 'rb').read()
-    return (response_status, response_headers, response_body)
-
-def request_global_downloads_page(env):
-    response_status = '200 OK'
-    response_headers = [('Content-Type', 'text/html')]
-    if debug.local_run(): response_body = open('global_downloads.angular', 'rb').read()
-    else: response_body = open(env['OPENSHIFT_REPO_DIR'] + 'global_downloads.angular', 'rb').read()
-    return (response_status, response_headers, response_body)
-
-
-def request_torrents(env):
-    query = cgi.parse(None, env)
-    if 'day_delta' in query.keys():
-        from torrents import torrents
-        response_status = '200 OK'
-        response_headers = [('Content-Type', 'application/json')]
-        response_body = torrents(int(query['day_delta'][0]))
-    else:
-        return BAD_REQUEST
-    return (response_status, response_headers, response_body)
-
-def request_global_downloads(env):
-    from torrents import global_downloads
-    response_status = '200 OK'
-    response_headers = [('Content-Type', 'application/json')]
-    response_body = global_downloads()
-    return (response_status, response_headers, response_body)
-
-def application(env, start_response):
-    allowed_requests = {
-        '/': request_root_page,
-        '/global': request_global_downloads_page,
-        '/torrents': request_torrents,
-        '/global_downloads': request_global_downloads,
-    }
-
-    request = env['PATH_INFO']
-    if request in allowed_requests.keys(): response_status, response_headers, response_body = allowed_requests[request](env)
-    else: response_status, response_headers, response_body = BAD_REQUEST
-
-    if type(response_body) != bytes:
-        response_body = response_body.encode('utf-8')
-    start_response(response_status, response_headers)
-    return [response_body]
-
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    for _ in range(2):
-        httpd.handle_request()
+#import debug, os, cgi, html
+#
+#BAD_REQUEST = ('400 Bad Request', [('Content-Type', 'text/html')], 'Nope!')
+#
+#def request_root_page(env):
+#    response_status = '200 OK'
+#    response_headers = [('Content-Type', 'text/html')]
+#    if debug.local_run(): response_body = open('torrents_by_day.angular', 'rb').read()
+#    else: response_body = open(env['OPENSHIFT_REPO_DIR'] + 'torrents_by_day.angular', 'rb').read()
+#    return (response_status, response_headers, response_body)
+#
+#def request_global_downloads_page(env):
+#    response_status = '200 OK'
+#    response_headers = [('Content-Type', 'text/html')]
+#    if debug.local_run(): response_body = open('global_downloads.angular', 'rb').read()
+#    else: response_body = open(env['OPENSHIFT_REPO_DIR'] + 'global_downloads.angular', 'rb').read()
+#    return (response_status, response_headers, response_body)
+#
+#
+#def request_torrents(env):
+#    query = cgi.parse(None, env)
+#    if 'day_delta' in query.keys():
+#        from torrents import torrents
+#        response_status = '200 OK'
+#        response_headers = [('Content-Type', 'application/json')]
+#        response_body = torrents(int(query['day_delta'][0]))
+#    else:
+#        return BAD_REQUEST
+#    return (response_status, response_headers, response_body)
+#
+#def request_global_downloads(env):
+#    from torrents import global_downloads
+#    response_status = '200 OK'
+#    response_headers = [('Content-Type', 'application/json')]
+#    response_body = global_downloads()
+#    return (response_status, response_headers, response_body)
+#
+#def application(env, start_response):
+#    allowed_requests = {
+#        '/': request_root_page,
+#        '/global': request_global_downloads_page,
+#        '/torrents': request_torrents,
+#        '/global_downloads': request_global_downloads,
+#    }
+#
+#    request = env['PATH_INFO']
+#    if request in allowed_requests.keys(): response_status, response_headers, response_body = allowed_requests[request](env)
+#    else: response_status, response_headers, response_body = BAD_REQUEST
+#
+#    if type(response_body) != bytes:
+#        response_body = response_body.encode('utf-8')
+#    start_response(response_status, response_headers)
+#    return [response_body]
+#
+#if __name__ == '__main__':
+#    from wsgiref.simple_server import make_server
+#    httpd = make_server('localhost', 8051, application)
+#    for _ in range(2):
+#        httpd.handle_request()
