e56632a0aba4121a19dfdc033153e74ce14f3549
dead-persimmon <kosyrin@gmail.com>
2014-08-03 07:23:11 -0400

    getting Flask to run...

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup(name='App', version='1.0', description='OpenShift App', author='Sashok', author_email='kosyrin@gmail.com', url='http://www.python.org/sigs/distutils-sig/')
+setup(
+    name = 'ATL',
+    version = '0.0',
+    description = 'OpenShift App',
+    author = 'Sashok',
+    author_email = 'kosyrin@gmail.com',
+    url = 'http://www.python.org/sigs/distutils-sig/',
+    install_requires = ['Flask']
+)
