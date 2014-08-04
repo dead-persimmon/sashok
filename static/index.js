19b416066d9da75627eb7f1cb4aeda12070d8e83
dead-persimmon <kosyrin@gmail.com>
2014-08-03 20:31:01 -0400

    prep work for smarter highlights

--- static/index.js
+++ static/index.js
@@ -3,25 +3,16 @@ var debug;
 var _app = angular.module('_app', []);
 
 _app.controller('_controller', ['$scope', '$http', function ($scope, $http) {
-    $scope.strings = {
-        displayNumGroupsPerDayHelp: 'Maximum number of groups to display in a single day.',
-    };
-
-    function CustomBooleanIn(value) { return !!value ? '1' : '0'; }
-    function CustomBooleanOut(value) { return !!Number(value); }
-    function CustomArrayIn(array) { return array.join('^_^'); }
-    function CustomArrayOut(string) { return string.length ? string.split('^_^') : []; }
-
     $scope.settings = {
         defaultValues: [
-            { name: 'orderByField', defaultValue: 'downloads', castIn: String, castOut: String },
-            { name: 'orderDescending', defaultValue: true, castIn: CustomBooleanIn, castOut: CustomBooleanOut },
-            { name: 'displayNumSeeders', defaultValue: false, castIn: CustomBooleanIn, castOut: CustomBooleanOut },
-            { name: 'displayNumLeechers', defaultValue: false, castIn: CustomBooleanIn, castOut: CustomBooleanOut },
-            { name: 'displayNumDownloads', defaultValue: true, castIn: CustomBooleanIn, castOut: CustomBooleanOut },
-            { name: 'displayNumGroupsPerDay', defaultValue: 15, castIn: String, castOut: Number },
-            { name: 'downloadDaysPerRequest', defaultValue: 2, castIn: String, castOut: Number },
-            { name: 'highlights', defaultValue: ['debug'], castIn: CustomArrayIn, castOut: CustomArrayOut },
+            { name: 'orderByField', defaultValue: 'downloads' },
+            { name: 'orderDescending', defaultValue: true },
+            { name: 'displayNumSeeders', defaultValue: false },
+            { name: 'displayNumLeechers', defaultValue: false },
+            { name: 'displayNumDownloads', defaultValue: true },
+            { name: 'displayNumGroupsPerDay', defaultValue: 15 },
+            { name: 'downloadDaysPerRequest', defaultValue: 2 },
+            { name: 'highlights', defaultValue: {} },
         ]
     };
 
@@ -30,12 +21,14 @@ _app.controller('_controller', ['$scope', '$http', function ($scope, $http) {
         $scope.settings.__defineGetter__(property.name, (function (property) {
             return function () {
                 var value = localStorage.getItem(property.name);
-                if (value != null) return property.castOut(localStorage.getItem(property.name));
+                if (value != null && value != '') return JSON.parse(value);
                 else return property.defaultValue;
             }
         })(property));
         $scope.settings.__defineSetter__(property.name, (function (property) {
-            return function (value) { localStorage.setItem(property.name, property.castIn(value)); }
+            return function (value) {
+                localStorage.setItem(property.name, JSON.stringify(value));
+            }
         })(property));
     }
 
@@ -43,18 +36,14 @@ _app.controller('_controller', ['$scope', '$http', function ($scope, $http) {
     
     $scope.pullTorrents = function (dayDelta) {
         var dayDelta = dayDelta || $scope.days.length;
-        if (!$scope.days[dayDelta]) $scope.days[dayDelta] = { data: [] };
-        $scope.days[dayDelta].loading = true;
-        $scope.days[dayDelta].failedToLoad = false;
+        if (!$scope.days[dayDelta]) $scope.days[dayDelta] = { data: [], status: 'loading', expanded: false };
         $http.get('/torrents/' + dayDelta)
             .success(function (data, status, headers, config) {
                 $scope.days[dayDelta].data = data.list;
-                $scope.days[dayDelta].loading = false;
+                $scope.days[dayDelta].status = 'ok';
             })
             .error(function (data, status, headers, config) {
-                console.log(data, status, headers, config);
-                $scope.days[dayDelta].failedToLoad = true;
-                $scope.days[dayDelta].loading = false;
+                $scope.days[dayDelta].staus = 'failed to load';
             });
     };
     
@@ -64,34 +53,41 @@ _app.controller('_controller', ['$scope', '$http', function ($scope, $http) {
     
     $scope.newHighlight = null;
     $scope.selectedHighlight = null;
+    $scope.highlightsCache = null;
     
-    $scope.deleteSelectedHighlight = function () {
-        var shl = $scope.selectedHighlight,
-            ahl = $scope.settings.highlights;
-        if (shl != null) {
-            var idx = ahl.indexOf(shl);
-            $scope.settings.highlights = ahl.slice(0, idx).concat(ahl.slice(idx + 1));
+    $scope.deleteHighlight = function () {
+        if ($scope.selectedHighlight != null) {
+            var updatedHighlights = {},
+                activeHighlights = $scope.settings.highlights;
+            for (var key in activeHighlights) {
+                updatedHighlights[key] = activeHighlights[key];
+            }
+            delete updatedHighlights[$scope.selectedHighlight];
+            $scope.settings.highlights = updatedHighlights;
             updateHighlightExpressions();
+            $scope.selectedHighlight = null;
         }
-        $scope.selectedHighlight = null;
     };
     
     $scope.createHighlight = function () {
         if ($scope.newHighlight != null) {
-            var highlight = $scope.newHighlight.trim();
-            if (highlight) {
-                $scope.settings.highlights = $scope.settings.highlights.concat(highlight);
+            var updatedHighlights = {},
+                activeHighlights = $scope.settings.highlights;
+            for (var key in activeHighlights) {
+                updatedHighlights[key] = activeHighlights[key];
             }
-            $scope.newHighlight = null;
+            updatedHighlights[$scope.newHighlight] = { lastDownloadedEpisode: 0 };
+            $scope.settings.highlights = updatedHighlights;
             updateHighlightExpressions();
+            $scope.newHighlight = null;
         }
     };
     
     var updateHighlightExpressions = function () {
+        $scope.highlightsCache = $scope.settings.highlights;
         $scope.highlightExpressions = [];
-        for (var index in $scope.settings.highlights) {
-            var tokens = $scope.settings.highlights[index];
-            tokens = tokens.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+        for (var key in $scope.highlightsCache) {
+            var tokens = key.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"); // escaping RegExp special chars
             tokens = tokens.split(' ');
             var expression = [];
             for (var i = 0; i < tokens.length; i += 1)
